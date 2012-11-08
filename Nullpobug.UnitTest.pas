fbdf5514e8f7afedf7392a152360f64e76652427
Shinya Okano <tokibito@gmail.com>
2012-11-09 01:23:22 +0900

    Changed: impl: Run test(RTTI method invoke) and Show test result.

--- Nullpobug.UnitTest.pas
+++ Nullpobug.UnitTest.pas
@@ -11,19 +11,7 @@ uses
 
 type
   EAssertionError = class(Exception);
-
-  TTestCase = class(TObject)
-  public
-    procedure SetUp; virtual;
-    procedure TearDown; virtual;
-    procedure AssertTrue(Value: Boolean);
-    procedure AssertFalse(Value: Boolean);
-    procedure AssertEquals(Value1, Value2: Integer); overload;
-    procedure AssertEquals(Value1, Value2: String); overload;
-    procedure Run;
-  end;
-
-  TTestCaseClass = class of TTestCase;
+  ESkipTest = class(Exception);
 
   TTestResultType = (rtOK, rtFail, rtError, rtSkip);
 
@@ -31,12 +19,36 @@ type
   private
     FResultType: TTestResultType;
     FError: Exception;
+    FTestMethodName: String;
+    FTestCaseName: String;
   public
     constructor Create;
     destructor Destroy; override;
     property ResultType: TTestResultType read FResultType write FResultType;
     property Error: Exception read FError write FError;
+    property TestMethodName: String read FTestMethodName write FTestMethodName;
+    property TestCaseName: String read FTestCaseName write FTestCaseName;
   end;
+
+  TOnRanTestMethod = procedure(TestResult: TTestResult) of object;
+
+  TTestCase = class(TObject)
+  private
+    FOnRanTestMethod: TOnRanTestMethod;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure SetUp; virtual;
+    procedure TearDown; virtual;
+    procedure AssertTrue(Value: Boolean);
+    procedure AssertFalse(Value: Boolean);
+    procedure AssertEquals(Value1, Value2: Integer); overload;
+    procedure AssertEquals(Value1, Value2: String); overload;
+    procedure Run(TestResultList: TObjectList<TTestResult>);
+    property OnRanTestMethod: TOnRanTestMethod read FOnRanTestMethod write FOnRanTestMethod;
+  end;
+
+  TTestCaseClass = class of TTestCase;
 
   TTestRunner = class(TObject)
   private
@@ -46,15 +58,17 @@ type
     constructor Create;
     destructor Destroy; override;
     procedure AddTestCase(TestCaseClass: TTestCaseClass);
-    function Run(TestCase: TTestCase): TTestResult;
+    procedure Run(TestCase: TTestCase); virtual;
     procedure RunTests; virtual;
+    property TestResultList: TObjectList<TTestResult> read FTestResultList;
   end;
 
   TTextTestRunner = class(TTestRunner)
   public
     procedure WriteHeader;
-    procedure WriteTestResult;
+    procedure WriteTestResult(TestResult: TTestResult);
     procedure WriteFooter;
+    procedure Run(TestCase: TTestCase); override;
     procedure RunTests; override;
   end;
 
@@ -67,6 +81,16 @@ var
 implementation
 
 { TestCase }
+constructor TTestCase.Create;
+begin
+  FOnRanTestMethod := nil;
+end;
+
+destructor TTestCase.Destroy;
+begin
+  FOnRanTestMethod := nil;
+end;
+
 procedure TTestCase.SetUp;
 begin
 end;
@@ -78,29 +102,72 @@ end;
 procedure TTestCase.AssertTrue(Value: Boolean);
 begin
   if not (Value = True) then
-    raise EAssertionError.CreateFmt('%s != True', [BoolToStr(Value, True)])
+    raise EAssertionError.CreateFmt('%s != True', [BoolToStr(Value, True)]);
 end;
 
 procedure TTestCase.AssertFalse(Value: Boolean);
 begin
   if not (Value = False) then
-    raise EAssertionError.CreateFmt('%s != False', [BoolToStr(Value, True)])
+    raise EAssertionError.CreateFmt('%s != False', [BoolToStr(Value, True)]);
 end;
 
 procedure TTestCase.AssertEquals(Value1, Value2: Integer);
 begin
   if not (Value1 = Value2) then
-    raise EAssertionError.CreateFmt('%d != %d', [Value1, Value2])
+    raise EAssertionError.CreateFmt('%d != %d', [Value1, Value2]);
 end;
 
 procedure TTestCase.AssertEquals(Value1, Value2: String);
 begin
   if not (Value1 = Value2) then
-    raise EAssertionError.CreateFmt('%s != %s', [Value1, Value2])
+    raise EAssertionError.CreateFmt('%s != %s', [Value1, Value2]);
 end;
 
-procedure TTestCase.Run;
+procedure TTestCase.Run(TestResultList: TObjectList<TTestResult>);
+var
+  RttiContext: TRttiContext;
+  RttiType: TRttiType;
+  Method: TRttiMethod;
+  TestResult: TTestResult;
 begin
+  RttiContext := TRttiContext.Create;
+  try
+    RttiType := RttiContext.GetType(ClassType);
+    for Method in RttiType.GetMethods do
+    begin
+      if LeftStr(Method.Name, 4) = 'Test' then
+      begin
+        TestResult := TTestResult.Create;
+        TestResult.ResultType := rtOk;
+        TestResult.TestMethodName := Method.Name;
+        TestResult.TestCaseName := ToString;
+        try
+          Method.Invoke(Self, []);
+        except
+          on E: EAssertionError do
+          begin
+            TestResult.ResultType := rtFail;
+            TestResult.Error := E;
+          end;
+          on E: ESkipTest do
+          begin
+            TestResult.ResultType := rtSkip;
+            TestResult.Error := E;
+          end;
+          on E: Exception do
+          begin
+            TestResult.ResultType := rtError;
+            TestResult.Error := E;
+          end;
+        end;
+        if Assigned(FOnRanTestMethod) then
+          FOnRanTestMethod(TestResult);
+        TestResultList.Add(TestResult);
+      end;
+    end;
+  finally
+    RttiContext.Free;
+  end;
 end;
 
 { TestResult }
@@ -135,36 +202,17 @@ begin
   FTestCaseList.Add(TestCaseClass.Create);
 end;
 
-function TTestRunner.Run(TestCase: TTestCase): TTestResult;
+procedure TTestRunner.Run(TestCase: TTestCase);
 begin
-  Result := TTestResult.Create;
-  try
-    TestCase.Run;
-    Result.ResultType := rtOk;
-  except
-    on E: EAssertionError do
-    begin
-      Result.ResultType := rtFail;
-      Result.Error := E;
-    end;
-    on E: Exception do
-    begin
-      Result.ResultType := rtError;
-      Result.Error := E;
-    end;
-  end;
+  TestCase.Run(TestResultList);
 end;
 
 procedure TTestRunner.RunTests;
 var
   TestCase: TTestCase;
-  TestResult: TTestResult;
 begin
   for TestCase in FTestCaseList do
-  begin
-    TestResult := Run(TestCase);
-    FTestResultList.Add(TestResult);
-  end;
+    Run(TestCase);
 end;
 
 { TTextTestRunner }
@@ -172,14 +220,39 @@ procedure TTextTestRunner.WriteHeader;
 begin
 end;
 
-procedure TTextTestRunner.WriteTestResult;
+procedure TTextTestRunner.WriteTestResult(TestResult: TTestResult);
+var
+  ResultMark: String;
 begin
+  if TestResult.ResultType = rtOK then
+    ResultMark := '.'
+  else
+    if TestResult.ResultType = rtFail then
+      ResultMark := 'F'
+    else
+      if TestResult.ResultType = rtError then
+        ResultMark := 'E'
+      else
+        if TestResult.ResultType = rtSkip then
+          ResultMark := 'S'
+        else
+          ResultMark := '?';
+  Write(ResultMark);
 end;
 
 procedure TTextTestRunner.WriteFooter;
 begin
+  WriteLn('');
   WriteLn(DupeString('-', 70));
+  WriteLn(Format('Ran %d tests in 0.000s', [TestResultList.Count]));
+  WriteLn('');
   WriteLn('OK');
+end;
+
+procedure TTextTestRunner.Run(TestCase: TTestCase);
+begin
+  TestCase.OnRanTestMethod := WriteTestResult;
+  inherited Run(TestCase);
 end;
 
 procedure TTextTestRunner.RunTests;
