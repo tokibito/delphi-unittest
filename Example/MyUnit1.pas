1f1b23ae9c420224b74a13c16f6dc611b801b95f
Shinya Okano <tokibito@gmail.com>
2012-11-10 03:24:58 +0900

    Changed: add test to example

--- Example/MyUnit1.pas
+++ Example/MyUnit1.pas
@@ -3,12 +3,18 @@ unit MyUnit1;
 interface
 
 function Add(A, B: Integer): Integer;
+function Sub(A, B: Integer): Integer;
 
 implementation
 
 function Add(A, B: Integer): Integer;
 begin
   Result := A + B;
+end;
+
+function Sub(A, B: Integer): Integer;
+begin
+  Result := A - B;
 end;
 
 end.
--- Example/MyUnit1Test.pas
+++ Example/MyUnit1Test.pas
@@ -11,6 +11,9 @@ type
   TMyUnit1Test = class(TTestCase)
   published
     procedure TestAdd;
+    procedure TestSub;
+    procedure TestAddFail;
+    procedure TestSkip;
   end;
 
 implementation
@@ -18,6 +21,22 @@ implementation
 procedure TMyUnit1Test.TestAdd;
 begin
   AssertEquals(Add(10, 20), 30);
+end;
+
+procedure TMyUnit1Test.TestSub;
+begin
+  AssertEquals(Sub(40, 20), 20);
+end;
+
+procedure TMyUnit1Test.TestAddFail;
+begin
+  // Fail Test
+  AssertEquals(Add(10, 20), 40);
+end;
+
+procedure TMyUnit1Test.TestSkip;
+begin
+  raise ESkipTest.Create('This is Skipped.');
 end;
 
 initialization
